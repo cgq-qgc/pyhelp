03f62f6588e8a37043e008420638cdaf408dd894
Jean-Sébastien Gosselin <jnsebgosselin@gmail.com>
2018-03-20 22:16:24 -0400

    :tada: first commit

---
+++ pyhelp/preprocessing.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+# Copyright © 2018 PyHelp Project Contributors
+# https://github.com/jnsebgosselin/pyhelp
+#
+# This file is part of PyHelp.
+# Licensed under the terms of the GNU General Public License.
+
+
