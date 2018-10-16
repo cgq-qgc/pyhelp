41d5fd16bb36f229bccc08ea2652e5c2355aa8c8
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2018-10-16 15:23:08 -0400

    Add copyright + rename app in init

--- pyhelp/__init__.py
+++ pyhelp/__init__.py
@@ -1,12 +1,16 @@
 # -*- coding: utf-8 -*-
 
+# Copyright © PyHelp Project Contributors
+# https://github.com/jnsebgosselin/pyhelp
+#
+# This file is part of PyHelp.
+# Licensed under the terms of the GNU General Public License.
 
 import os
 
-
 version_info = (0, 1, 0, 'dev')
 __version__ = '.'.join(map(str, version_info))
-__appname__ = 'pyhelp'
+__appname__ = 'PyHelp'
 __namever__ = __appname__ + " " + __version__
 __date__ = '29/01/2018'
 __project_url__ = "https://github.com/jnsebgosselin/pyhelp"
