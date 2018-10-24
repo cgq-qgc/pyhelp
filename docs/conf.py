be60493737384f168d7e914e6ea07d7844bd6db6
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2018-10-24 14:49:13 -0400

    mock pytables in conf

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,7 @@ class Mock(MagicMock):
         return MagicMock()
 
 
-MOCK_MODULES = ['pyhelp.HELP3O']
+MOCK_MODULES = ['pyhelp.HELP3O', 'pytables']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # -- Project information -----------------------------------------------------
