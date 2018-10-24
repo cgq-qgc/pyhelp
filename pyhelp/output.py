93042ca47c7c78cb82192649e16f4f61a545a369
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2018-10-24 13:09:58 -0400

    Add plot_area_monthly_avg
    Plot water budget average monthly values for the study area.

--- pyhelp/output.py
+++ pyhelp/output.py
@@ -78,7 +78,7 @@ class HelpOutput(Mapping):
 
     def calc_area_monthly_avg(self):
         """
-        Calcul water budget average monthly values over the study area.
+        Calcul water budget average monthly values for the study area.
 
         Return a dictionary that contains a 2D numpy array for each
         component of the water budget with average values calculated over
@@ -92,3 +92,68 @@ class HelpOutput(Mapping):
         else:
             return {key: np.nansum(self.data[key], axis=0)/Np for key in keys}
 
+    def plot_area_monthly_avg(self, figname=None):
+        """
+        Plot water budget average monthly values for the study area.
+
+        Plot the monthly average values calculated for each component of the
+        water budget for the entire study area.
+        """
+        fwidth, fheight = 9, 6.5
+        fig, ax = plt.subplots()
+        fig.set_size_inches(fwidth, fheight)
+
+        # Setup axe margins :
+        left_margin = 1.5/fwidth
+        right_margin = 0.25/fwidth
+        top_margin = 1/fheight
+        bot_margin = 0.7/fheight
+        ax.set_position([left_margin, bot_margin,
+                         1 - left_margin - right_margin,
+                         1 - top_margin - bot_margin])
+
+        avg_monthly = self.calc_area_monthly_avg()
+        months = range(1, 13)
+        l1, = ax.plot(months, np.mean(avg_monthly['precip'], axis=0),
+                      marker='o', mec='white', clip_on=False, lw=2)
+        l2, = ax.plot(months, np.mean(avg_monthly['rechg'], axis=0),
+                      marker='o', mec='white', clip_on=False, lw=2)
+        l3, = ax.plot(months, np.mean(avg_monthly['runoff'], axis=0),
+                      marker='o', mec='white', clip_on=False, lw=2)
+        l4, = ax.plot(months, np.mean(avg_monthly['evapo'], axis=0),
+                      marker='o', mec='white', clip_on=False, lw=2)
+        l5, = ax.plot(months, np.mean(avg_monthly['subrun1'], axis=0),
+                      marker='o', mec='white', clip_on=False, lw=2)
+        l6, = ax.plot(months, np.mean(avg_monthly['subrun2'], axis=0),
+                      marker='o', mec='white', clip_on=False, lw=2)
+
+        ax.set_ylabel('Composantes du bilan hydrologique\n(mm/mois)',
+                      fontsize=16, labelpad=10)
+        ax.set_xlabel('Mois', fontsize=16, labelpad=10)
+        ax.axis(ymin=-5, ymax=140)
+        ax.grid(axis='both', color=[0.35, 0.35, 0.35], ls='-', lw=0.5)
+        ax.set_xticks(months)
+        ax.set_xticklabels(['Jan', 'Fév', 'Mar', 'Avr', 'Mai', 'Jun', 'Jul',
+                            'Aoû', 'Sep', 'Oct', 'Nov', 'Déc'])
+        ax.tick_params(axis='both', direction='out', labelsize=12)
+
+        lines = [l1, l2, l3, l4, l5, l6]
+        labels = ["Précipitations totales", "Recharge au roc",
+                  "Ruissellement de surface", "Évapotranspiration",
+                  "Ruissellement hypodermique superficiel",
+                  "Ruissellement hypodermique profond"]
+        legend = ax.legend(lines, labels, numpoints=1, fontsize=12,
+                           borderaxespad=0, loc='lower left', borderpad=0.5,
+                           bbox_to_anchor=(0, 1), ncol=2)
+        legend.draw_frame(False)
+
+        # if figname is not None:
+        #     fig.savefig('bilan_hydro_mensuel_%s.pdf' % figname_sufix)
+
+
+if __name__ == "__main__":
+    output_fpath = "C:/Users/User/pyhelp/example/help_example.out"
+    hout = HelpOutput(output_fpath)
+    data = hout.data
+    grid = hout.grid
+    hout.plot_avg_monthly_budget()
