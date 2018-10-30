c46da726b9fc91b38c8ffc304798fd4adcf14ce5
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2018-10-30 13:20:17 -0400

    Add channel priority to conda

--- pyhelp/tests/test_helpmanager.py
+++ pyhelp/tests/test_helpmanager.py
@@ -65,7 +65,6 @@ def test_calc_help_cells(helpm):
     help_output_hdf5 = osp.join(helpm.workdir, 'help_example.out')
     helpm.build_help_input_files()
     helpm.calc_help_cells(help_output_hdf5, cellnames)
-
     assert osp.exists(help_output_hdf5)
 
 
