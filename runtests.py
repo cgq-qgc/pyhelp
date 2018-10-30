9116e96fed55284b1ba35629b166b5e732e261d6
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2018-10-30 09:57:12 -0400

    First commit

---
+++ runtests.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+# Copyright © PyHelp Project Contributors
+# https://github.com/jnsebgosselin/pyhelp
+#
+# This file is part of PyHelp.
+# Licensed under the terms of the GNU General Public License.
+
+"""
+File for running tests programmatically.
+"""
+
+import pytest
+
+
+def main():
+    """
+    Run pytest tests.
+    """
+    errno = pytest.main(['-x', 'pyhelp',  '-v', '-rw', '--durations=10',
+                         '--cov=pyhelp'])
+    if errno != 0:
+        raise SystemExit(errno)
+
+
+if __name__ == '__main__':
+    main()
