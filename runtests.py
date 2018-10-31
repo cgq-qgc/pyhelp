398a4c20ae2bde413c3586a86d3dafa4c09aeff1
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2018-10-31 01:14:52 -0400

    Add mingw bin to sys.path when runnung tests

--- runtests.py
+++ runtests.py
@@ -9,7 +9,7 @@
 """
 File for running tests programmatically.
 """
-
+import sys
 import pytest
 
 
@@ -17,6 +17,9 @@ def main():
     """
     Run pytest tests.
     """
+    sys.path.insert(
+        0, "C:\\mingw-w64\\x86_64-7.2.0-posix-seh-rt_v5-rev1\\mingw64\\bin")
+
     errno = pytest.main(['-x', 'pyhelp',  '-v', '-rw', '--durations=10',
                          '--cov=pyhelp'])
     if errno != 0:
