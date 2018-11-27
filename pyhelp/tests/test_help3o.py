b2c27da1f126c57cefe1d78419676ff1f657566b
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2018-11-27 00:57:27 -0500

    Remove blanks

--- pyhelp/tests/test_help3o.py
+++ pyhelp/tests/test_help3o.py
@@ -82,7 +82,7 @@ def test_run_help_singlecell(rca_params):
     cellname, results = run_help_singlecell(('rca', rca_params))
     assert not osp.exists(rca_params[5])
     assert cellname == 'rca'
-    
+
     # Precipitations.
     precip = np.sum(results['precip'] * 0.0393701, axis=1)
     for i, expected_result in enumerate([48.5, 58.32, 56.71]):
@@ -97,23 +97,23 @@ def test_run_help_singlecell(rca_params):
     rechg = np.sum(results['rechg'] * 0.0393701, axis=1)
     for i, expected_result in enumerate([0.000004, 0.000006, 0.000007]):
         assert abs(rechg[i] - expected_result) < 0.1, 'rechg year %i' % i
-        
+
     # Evapotranspiration.
     evapo = np.sum(results['evapo'] * 0.0393701, axis=1)
     for i, expected_result in enumerate([32.178, 33.420, 36.540]):
         assert abs(evapo[i] - expected_result) < 0.1, 'evapo year %i' % i
-        
+
     # Superficial subsurface runoff.
     subrun1 = np.sum(results['subrun1'] * 0.0393701, axis=1)
     for i, expected_result in enumerate([15.4971, 22.8698, 19.1360]):
         assert abs(subrun1[i] - expected_result) < 0.1, 'subrun1 year %i' % i
-        
+
     # Deep subsurface runoff.
     subrun2 = np.sum(results['subrun2'] * 0.0393701, axis=1)
     expected_results = (0.0543 + 0.0833, 0.1481 + 0.1658, 0.1835 + 0.1935)
     for i, expected_result in enumerate(expected_results):
         assert abs(subrun2[i] - expected_result) < 0.1, 'subrun2 year %i' % i
-    
+
 
 if __name__ == '__main__':
     pytest.main(['-x', os.path.basename(__file__), '-v', '-rw'])
