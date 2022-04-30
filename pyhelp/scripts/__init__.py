112646b5beef42589e2a47b318f1d59c3c35d736
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2022-04-30 03:15:16 -0400

    Delete init from tests and scripts

--- pyhelp/scripts/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# -----------------------------------------------------------------------------
-# Copyright © PyHelp Project Contributors
-# https://github.com/cgq-qgc/pyhelp
-#
-# This file is part of PyHELP.
-# Licensed under the terms of the MIT License.
-# -----------------------------------------------------------------------------
