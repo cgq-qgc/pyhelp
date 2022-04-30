9de5e0800cef273736aaadd854a219656d75c9f1
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2022-04-30 09:34:28 -0400

    Revert "Delete init from tests and scripts"
    This reverts commit 112646b5beef42589e2a47b318f1d59c3c35d736.

---
+++ pyhelp/scripts/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# Copyright © PyHelp Project Contributors
+# https://github.com/cgq-qgc/pyhelp
+#
+# This file is part of PyHELP.
+# Licensed under the terms of the MIT License.
+# -----------------------------------------------------------------------------
