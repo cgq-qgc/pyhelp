56ef4c8cfd39e678b57513446353c51ec244a075
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2026-02-26 13:21:35 -0500

    Update test_help3o.py

--- pyhelp/tests/test_help3o.py
+++ pyhelp/tests/test_help3o.py
@@ -38,11 +38,16 @@ def rca_params(rca_folder, tmp_path):
     summary_out = 0
     tfsoil = 32.0  # Must be in Fahrenheit
     simu_nyear = 3
+
+    with open(osp.join(rca_folder, 'RCRA.D10'), 'r') as f:
+        raw_lines = f.read().splitlines()
+    d10_input = np.char.ljust(raw_lines, 80).astype('S80')
+
     return (osp.join(rca_folder, 'RCRA.D4'),
             osp.join(rca_folder, 'RCRA.D7'),
             osp.join(rca_folder, 'RCRA.D13'),
             osp.join(rca_folder, 'RCRA.D11'),
-            osp.join(rca_folder, 'RCRA.D10'),
+            d10_input,
             osp.join(tmp_path, 'NEW_RCA.OUT'),
             daily_out,
             monthly_out,
