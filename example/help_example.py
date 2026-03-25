566d41fab895f9abd212abd30833caee3505b633
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2026-03-25 13:17:59 -0400

    Update help_example.py

--- example/help_example.py
+++ example/help_example.py
@@ -3,12 +3,12 @@
 This example shows how to use PyHELP to calculate the monthly water balance
 for a section of the Rivière du Nord watershed in the Laurentians, Quebec, Can.
 
-Updated for PyHELP version 0.3.1
+Updated for PyHELP version 0.5.0
 """
 
-import os.path as osp
+from pathlib import Path
 import pandas as pd
-from pyhelp.managers import HelpManager
+from pyhelp import __rootdir__, HelpManager
 import pyhelp.bilan as HelpBilan
 
 if __name__ == '__main__':
@@ -17,17 +17,19 @@ if __name__ == '__main__':
     #    https://docs.python.org/3.6/library/
     #    multiprocessing.html#programming-guidelines
 
-    # Define the working directory.
-    workdir = "D:/Projets/pyhelp/example/"
+    # Define the working directory here.
+    workdir = Path(__rootdir__) / 'example'
+    print(workdir)
 
     # Instantiate the HelpManager and provide the paths to the grid and
     # weather input data files so that they are loaded automatically.
     helpm = HelpManager(
-        workdir,
-        path_to_grid=workdir + 'input_grid.csv',
-        path_to_precip=workdir + 'precip_input_data.csv',
-        path_to_airtemp=workdir + 'airtemp_input_data.csv',
-        path_to_solrad=workdir + 'solrad_input_data.csv')
+        str(workdir),
+        path_to_grid=str(workdir / 'input_grid.csv'),
+        path_to_precip=str(workdir / 'precip_input_data.csv'),
+        path_to_airtemp=str(workdir / 'airtemp_input_data.csv'),
+        path_to_solrad=str(workdir / 'solrad_input_data.csv')
+        )
 
     # Note that you can access the grid input data through
     # the 'grid' attribute of the HelpManager.
@@ -35,6 +37,8 @@ if __name__ == '__main__':
     # Note that you can access the weather input data through the
     # 'precip_data', 'airtemp_data', and 'solrad_data' attributes
     # of the HelpManager.
+
+    # %%
 
     # =========================================================================
     # Run HELP simulation for all the cells in cellnames.
@@ -50,20 +54,27 @@ if __name__ == '__main__':
     # Note that the monthly output data will be automatically saved to
     # the HDF5 file define in filepath.
     output_help = helpm.calc_help_cells(
-        path_to_hdf5=workdir + 'help_example.out',
+        path_to_hdf5=str(workdir / 'help_example.out'),
         cellnames=cellnames,
         tfsoil=-3,
         sf_edepth=0.15,
         sf_ulai=1,
-        sf_cn=1.15)
+        sf_cn=1.15,
+        # Don't write D10/D11 HELP30 input files. See the documentation.
+        write_help_input_files=False,
+        # Don' write HELP30 .OUT files. See the documentation.
+        write_help_output_files=False,
+        )
 
     # Export and save annual averages of HELP output values to a csv file.
-    output_help.save_to_csv(osp.join(workdir, 'help_example_yearly.csv'))
+    output_help.save_to_csv(workdir / 'help_example_yearly.csv')
 
     # Plot some results.
     output_help.plot_area_monthly_avg(fig_title="PyHELP Example")
     output_help.plot_area_yearly_avg(fig_title="PyHELP Example")
     output_help.plot_area_yearly_series(fig_title="PyHELP Example")
+
+    # %%
 
     # =========================================================================
     # Compare with river total and base streamflow
@@ -73,11 +84,12 @@ if __name__ == '__main__':
     output_surf = helpm.calc_surf_water_cells(
         cellnames=cellnames,
         evp_surf=650,
-        path_outfile=osp.join(workdir, 'surf_example.out'))
+        path_outfile=workdir / 'surf_example.out'
+        )
 
     # Read observed yearly total and base streamflow (in mm/year).
     obs_qflow = pd.read_csv(
-        osp.join(workdir, 'obs_yearly_river_flow.csv'),
+        workdir / 'obs_yearly_river_flow.csv',
         index_col=0)
 
     # Calcul simulated early total and base streamflow (in mm/year).
